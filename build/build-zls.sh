237e3034e3509abc81a55bf16b052d6e2876209e
Mark Stewart <mark@adamar.ca>
2021-09-05 06:00:50 -0400

    Use main zls branch

--- build/build-zls.sh
+++ build/build-zls.sh
@@ -4,12 +4,8 @@ set -xe
 export PATH=$PATH:/root/.local/bin
 
 cd;
-# main ZLS is broken
-# git clone --recurse-submodules https://github.com/zigtools/zls
-git clone --recurse-submodules https://github.com/sagehane/zls
+git clone --recurse-submodules https://github.com/zigtools/zls
 cd zls
-git checkout submodule
-git submodule update
 zig build -Drelease-safe
 printf 'y\nVSCode\ny\ny\ny\ny\n' | ./zig-out/bin/zls config
 
