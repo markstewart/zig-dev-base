a8e7740d5dd9c418bb5de819b3de1f05572951bd
Mark Stewart <mark@adamar.ca>
2021-10-10 11:15:07 -0400

    Fix changes in bundle naming

--- build/get-llvm.sh
+++ build/get-llvm.sh
@@ -2,10 +2,9 @@ set -xe
 cd;
 
 CLANG_VERSION=13.0.0
-CLANG_EXPORT_DIR_NAME=clang+llvm-${CLANG_VERSION}-x86_64-linux-gnu-ubuntu-;
-CLANG_BUNDLE_NAME=${CLANG_EXPORT_DIR_NAME}16.04;
+CLANG_BUNDLE_NAME=clang+llvm-${CLANG_VERSION}-x86_64-linux-gnu-ubuntu-16.04;
 
-curl -o $CLANG_BUNDLE_NAME.tar.xz -L https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.1/$CLANG_BUNDLE_NAME.tar.xz;
+curl -o $CLANG_BUNDLE_NAME.tar.xz -L https://github.com/llvm/llvm-project/releases/download/llvmorg-${CLANG_VERSION}/$CLANG_BUNDLE_NAME.tar.xz;
 tar -xvf ./$CLANG_BUNDLE_NAME.tar.xz;
 rm ./$CLANG_BUNDLE_NAME.tar.xz;
-mv $CLANG_EXPORT_DIR_NAME .local;
+mv $CLANG_BUNDLE_NAME .local;
