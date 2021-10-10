0e5c3bdd854bd6bca9e2ba47bad94485b1579982
Mark Stewart <mark@adamar.ca>
2021-10-10 11:07:13 -0400

    Update clang version

--- build/get-llvm.sh
+++ build/get-llvm.sh
@@ -1,7 +1,8 @@
 set -xe
 cd;
 
-CLANG_EXPORT_DIR_NAME=clang+llvm-12.0.1-x86_64-linux-gnu-ubuntu-;
+CLANG_VERSION=13.0.0
+CLANG_EXPORT_DIR_NAME=clang+llvm-${CLANG_VERSION}-x86_64-linux-gnu-ubuntu-;
 CLANG_BUNDLE_NAME=${CLANG_EXPORT_DIR_NAME}16.04;
 
 curl -o $CLANG_BUNDLE_NAME.tar.xz -L https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.1/$CLANG_BUNDLE_NAME.tar.xz;
